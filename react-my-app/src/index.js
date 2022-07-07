e5323998772ed5f9d55d5ab405f2da3c8f1d09e0
Summer498 <h262sawamura@i.softbank.jp>
2022-07-07 21:37:28 +0900

    add some experiments

--- react-my-app/src/index.js
+++ react-my-app/src/index.js
@@ -4,11 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const range = (b, n, s = 1) => [...Array(n)].map((_, i) => b + i * s);
+const order = (n) => [...Array(n)].map((_, i) => i);
+
+const Hoge = () =>
+	<div>
+		{range(0, 3, 3).map(e =>
+			<div>
+				{range(e, 3).map(e => <li>item{e}</li>)}
+			</div>
+		)}
+	</div>
+
+const Rects = ()=>
+	<svg>
+		{range(0, 3).map(i =>
+			<g>
+				{range(0, 3).map(j => <rect x={String(j*100)} y={String(i*100)} width="80" height="80" />)}
+			</g>
+		)}
+	</svg>
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+		<Hoge />
+		<Rects />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
