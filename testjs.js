336d9886ec39ab7bdd7e144e77190b72c9fe6b72
Summer498 <h262sawamura@i.softbank.jp>
2022-07-14 11:14:44 +0900

    extend TPS function

--- testjs.js
+++ testjs.js
@@ -22,71 +22,17 @@ for (let i = 0; i < 3; i++) {
 const answer = document.getElementById("answer").innerHTML;
 
 
-
+const assert = b => console.assert(b);
+const not = b => !b;
 const mod = (x, m) => (x % m + m) % m;
-const range = (b, n, s = 1) => [...Array(n)].map((_, k) => k * s + b);
-const zeros = (n) => [...Array(n)].map(e => 0);
-
-Array.prototype.ringShift = function (b) {
-	const l = this.length;
-	const bm = mod(b, l);
-	return this.concat(this).slice(l - bm, 2 * l - bm);
-};
-
-const arr2onehot = (arr, n = 0) => [...Array(Math.max(Math.max(...arr) + 1, n))].map((_, k) => (arr.indexOf(k) + 1 ? 1 : 0));
-const arr2onehotInMod = (arr, m = 1) => arr2onehot(arr.map(e => mod(e, m)), m);
-const vAdd = (v1, v2) => {
-	console.assert(v1.length == v2.length);
-	return v1.map((v, k) => v + v2[k]);
-};
+const range = (b, n, s = 1) => [...Array(n)].map((_, i) => i * s + b);
+const zeros = n => [...Array(n)].map(e => 0);
+const vAdd = (a, b) => (assert(a.length == b.length), a.map((_, i) => a[i] + b[i]));
 const vSum = (...arrs) => {
-	let sum = zeros(arrs[0].length);
-	arrs.forEach(arr => sum = vAdd(sum, arr));
-	return sum;
+	let s = zeros(arrs[0].length);
+	arrs.forEach(arr => s = vAdd(s, arr));
+	return s;
 };
-
-const name2BS = (name) => range(0, 12).map((_, k) => (
-	{
-		"maj": { 0: 4, 1: 0, 2: 0, 3: 0, 4: 2, 5: 0, 6: 0, 7: 3, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0 },
-		"min": { 0: 4, 1: 0, 2: 0, 3: 2, 4: 0, 5: 0, 6: 0, 7: 3, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0 },
-		"aug": { 0: 4, 1: 0, 2: 0, 3: 0, 4: 2, 5: 0, 6: 0, 7: 0, 8: 3, 9: 0, 10: 0, 11: 0, 12: 0 },
-		"dim": { 0: 4, 1: 0, 2: 0, 3: 2, 4: 0, 5: 0, 6: 3, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0 },
-		"sus2": { 0: 4, 1: 0, 2: 2, 3: 0, 4: 0, 5: 0, 6: 0, 7: 3, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0 },
-		"sus4": { 0: 4, 1: 0, 2: 0, 3: 0, 4: 0, 5: 2, 6: 0, 7: 3, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0 },
-		"7": { 0: 4, 1: 0, 2: 0, 3: 0, 4: 2, 5: 0, 6: 0, 7: 3, 8: 0, 9: 0, 10: 2, 11: 0, 12: 0 },
-		"M7": { 0: 4, 1: 0, 2: 0, 3: 0, 4: 2, 5: 0, 6: 0, 7: 3, 8: 0, 9: 0, 10: 0, 11: 2, 12: 0 },
-		"m7": { 0: 4, 1: 0, 2: 0, 3: 2, 4: 0, 5: 0, 6: 0, 7: 3, 8: 0, 9: 0, 10: 2, 11: 0, 12: 0 },
-	}[name][k]
-));
-
-function triCode2BS(key, root, name) {
-	return name2BS(name).ringShift(root).map((v, k) =>
-		v ? v : [...Array(12)].map((_, k) => (
-			{
-				"maj": [0, 2, 4, 5, 7, 9, 11],
-				"min": [0, 2, 3, 5, 7, 8, 10],
-			}[key.slice(1, key.length)]
-				.find(e => e == k)) ? 1 : 0)[((k - key[0]) % 12 + 12) % 12]
-	);
-}
-
-console.log("C, Cmaj", triCode2BS([0, "maj"], 0, "maj"));
-console.log("C, Gmaj", triCode2BS([0, "maj"], 7, "maj"));
-console.log("G, Cmaj", triCode2BS([7, "maj"], 0, "maj"));
-console.log("Cmin, Cm", triCode2BS([0, "min"], 0, "min"));
-
-/*
-こんな風に書きたい
-const C = chord.C.maj
-const Cm = chord.C.minor
-const Cdim = chord.C.dim
-const Caug = chord.C.aug
-const Csus2 = chord.C.sus2
-const Csus4 = chord.C.sus4
-const CM7 = chord.C.major7
-const C7 = chord.C.seventh
-const Cm7 = chord.C.minor7
-*/
 
 /**
  * @brief generate array
@@ -97,30 +43,15 @@ const Cm7 = chord.C.minor7
  */
 const genArr = (n, f) => [...Array(n)].map((_, i) => f(i));
 
-(() => {
-	const key_quality = 0;
-	const key = 0;
-	const degree = 1;
-	const alt5 = 1;
-	console.assert(degree);
+Boolean.prototype.toNumber = function () { return this ? 1 : 0; };
+Array.prototype.onehot = function (n = 0) { return [...Array(Math.max(Math.max(...this) + 1, n))].map((_, i) => (this.includes(i)).toNumber()); };
+Array.prototype.onehotInMod = function (m = 1) { return this.map(e => mod(e, m)).onehot(m); };
+Array.prototype.remove = function (rmv) { return this.filter(e => not(rmv.includes(e))); };
+Array.prototype.ringShift = function (b) {
+	const l = this.length, bm = mod(b, l);
+	return this.concat(this).slice(l - bm, 2 * l - bm);
+};
 
-	const maj = [0, 2, 4, 5, 7, 9, 11];
-	const min = [0, 2, 3, 5, 7, 8, 10];
-	const s = [maj, min][key_quality].map(e => e + key);
-	s[4] += alt5;
-
-	const c = s.filter((_, k) => genArr(3, i => mod(degree - 1 + i * 2, 7)).indexOf(k) + 1);
-	const leveld = arr2onehotInMod(s, 12);
-	const levelc = arr2onehot(c, 12);
-	const levelb = arr2onehot([s[0], s[4]], 12);
-	const levela = arr2onehot([s[0]], 12);
-
-	console.log(leveld);
-	console.log(levelc);
-	console.log(levelb);
-	console.log(levela);
-	console.log(vSum(leveld, levelc, levelb, levela));
-})();
 
 const Key_quality = {
 	major: [0, 2, 4, 5, 7, 9, 11],
@@ -138,14 +69,18 @@ const Chroma = {
 };
 
 const Chord_index = {
-	seventh: 0,
-	dominant7: 0,
-	minor7: 1,
-	major7: 2,
-	ninth: 3,
-	added6: 4,
-	added46: 5,
-}
+	none: { rmv: [], add: [] },
+	seventh: { rmv: [], add: [7] },
+	ninth: { rmv: [], add: [7, 9] },
+	added6: { rmv: [5], add: [6] },
+	added46: { rmv: [3, 5], add: [4, 6] },
+};
+
+const Alt5 = {
+	dim5: -1,
+	none: 0,
+	aug5: 1
+};
 
 //TODO: +46, +6 要る?
 /**
@@ -157,16 +92,18 @@ const Chord_index = {
  * @param alt5 of chord in the key: one of {-1, 0, 1} (default:0)
  * @return Basic Space of chord
  */
-const get_BS = (key, key_quality, degree, indexes = 0, alt5 = 0) => {
-	console.assert(degree);
-	const s = key_quality.map(e => e + key);
+const get_BS = (key, key_quality, degree, indexes = Chord_index.none, alt5 = 0) => {
+	assert(degree);
+	const d = degree;
+	const s = key_quality.map(e => mod(e + key, 12));
 	s[4] += alt5;
 
-	const c = s.filter((_, k) => genArr(3, i => mod(degree - 1 + i * 2, 7)).indexOf(k) + 1);
-	const leveld = arr2onehotInMod(s, 12);
-	const levelc = arr2onehotInMod(c, 12);
-	const levelb = arr2onehotInMod([s[0], s[4]], 12);
-	const levela = arr2onehotInMod([s[0]], 12);
+	const code_tone = genArr(3, i => 2 * i + 1).remove(indexes.rmv).concat(indexes.add);
+	const c = genArr(code_tone.length, i => s[mod(d + code_tone[i] - 2, 7)]);
+	const leveld = s.onehot(12);
+	const levelc = c.onehot(12);
+	const levelb = [c[0], c[2]].onehot(12);
+	const levela = [c[0]].onehot(12);
 	return vSum(leveld, levelc, levelb, levela);
 };
 
@@ -199,7 +136,11 @@ const BS_dist = (src, dst) => {
 };
 
 const Cmaj = get_BS(Chroma.C, Key_quality.major, 1);
-const Gmaj = get_BS(Chroma.G, Key_quality.major, 4);
-console.log(Cmaj, Gmaj);
+const Gmaj = get_BS(Chroma.C, Key_quality.major, 5);
+const G7 = get_BS(Chroma.C, Key_quality.major, 5, Chord_index.seventh);
+const F46 = get_BS(Chroma.C, Key_quality.major, 4, Chord_index.added46);
+console.log(Cmaj, Gmaj, G7, F46);
 console.log(BS_dist(Cmaj, Gmaj));
 console.log(BS_dist(Gmaj, Cmaj));
+console.log(BS_dist(Cmaj, G7));
+console.log(BS_dist(G7, Cmaj));
