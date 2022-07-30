755a9d8dcd4b9ae6d52b75768fb62632a0bd78f0
Summer498 <h262sawamura@i.softbank.jp>
2022-07-30 09:32:03 +0900

    add dom training

--- testjs.js
+++ testjs.js
@@ -19,6 +19,49 @@ for (let i = 0; i < 3; i++) {
 	};
 }
 
-const answer = document.getElementById("answer").innerHTML;
+const answer = document.getElementById("answer");
 
+/*
+<div class = "people" id = "Fantastic Party">
+	<div class = "person" id = "Adam"></div>
+	<div class = "person" id = "Ben">
+		<div class = "bag" id = "gorgeous bag">
+	</div>
+	<div class = "person" id = "Charlie"></div>
+	<div class = "person" id = "David"></div>
+</div>
+*/
+
+const Multiplicate = e => (e instanceof Array) ? e : [e]
+const d = document
+d.create = function (tag) {	return this.createElement(tag) }
+Element.prototype.set = function (attributes){
+	for (const key in attributes){ this.setAttribute(key, attributes[key]); }
+	return this;
+} 
+Element.prototype.setText = function (text){
+	this.appendChild(document.createTextNode(text));
+	return this;
+}
+Element.prototype.append = function (elements){
+	for(const e of Multiplicate(elements)){ this.appendChild(e); }
+	return this;
+}
+//*
+const DIV = () => document.createElement("div");
+const HTML = {};
+HTML.div = () => document.createElement("div");
+
+
+answer.appendChild(
+	HTML.div().set({ class: "people", id: "Fantastic Party" }).setText("Fantastic Party です").append([
+		HTML.div().set({ class: "person", id: "Adam" }).setText("Adam です"),
+		HTML.div().set({ class: "person", id: "Ben" }).setText("Ben です").append(
+			HTML.div().set({ class: "bag", id: "gorgeous bag" }).setText("Ben の豪華なバッグ です"),
+		),
+		HTML.div().set({ class: "person", id: "Charlie" }).setText("Charlie です"),
+		HTML.div().set({ class: "person", id: "David" }).setText("David です"),
+	])
+);
+//*/
 
